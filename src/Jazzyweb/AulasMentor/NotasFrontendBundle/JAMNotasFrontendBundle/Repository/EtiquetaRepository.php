ee4016f5534d934a8ee8d327f1d36e54ccf8c1fa
JT <tejadong@gmail.com>
2017-07-25 21:35:29 +0200

    Creados los métodos "findByUsuarioOrderedByTexto" y "findOneByTextoAndUsuario" en el repositorio de la entidad Etiqueta.

--- src/Jazzyweb/AulasMentor/NotasFrontendBundle/JAMNotasFrontendBundle/Repository/EtiquetaRepository.php
+++ src/Jazzyweb/AulasMentor/NotasFrontendBundle/JAMNotasFrontendBundle/Repository/EtiquetaRepository.php
@@ -3,6 +3,8 @@
 namespace Jazzyweb\AulasMentor\NotasFrontendBundle\JAMNotasFrontendBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Jazzyweb\AulasMentor\NotasFrontendBundle\JAMNotasFrontendBundle\Entity\Etiqueta;
+use Jazzyweb\AulasMentor\NotasFrontendBundle\JAMNotasFrontendBundle\Entity\Usuario;
 
 /**
  * EtiquetaRepository
@@ -12,4 +14,42 @@ use Doctrine\ORM\EntityRepository;
  */
 class EtiquetaRepository extends EntityRepository
 {
+    public function findByUsuarioOrderedByTexto($username)
+    {
+        $query = $this
+            ->getEntityManager()
+            ->createQuery(
+                "SELECT e FROM JAMNotasFrontendBundle:Etiqueta e
+              JOIN  e.usuario u where u.id = :username ORDER BY e.texto ASC")
+            ->setParameters(array('username' => $username));
+
+        return $query->getResult();
+    }
+
+    public function findOneByTextoAndUsuario($etiqueta, $usuario)
+    {
+        if ($usuario instanceof Usuario) {
+            $username = $usuario->getUsername();
+        } else {
+            $username = $usuario;
+        }
+
+        if ($etiqueta instanceof Etiqueta) {
+            $id_etiqueta = $etiqueta->getId();
+        } else {
+            $id_etiqueta = $etiqueta;
+        }
+
+        $query = $this->getEntityManager()
+            ->createQuery(
+                "SELECT e FROM JAMNotasFrontendBundle:Etiqueta e
+              JOIN e.usuario u
+              WHERE e.texto = :id_etiqueta and u.username=:username")
+            ->setParameters(array(
+                    'username' => $username,
+                    'id_etiqueta' => $id_etiqueta)
+            );
+
+        return $query->getOneOrNullResult();
+    }
 }
