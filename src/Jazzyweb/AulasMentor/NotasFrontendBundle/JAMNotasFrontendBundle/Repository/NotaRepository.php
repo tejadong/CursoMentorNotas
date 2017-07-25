9d7462feb209480a0f9fdcca9514c39fe05c2e4b
JT <tejadong@gmail.com>
2017-07-25 21:36:03 +0200

    Creados los métodos "findByUsuarioAndEtiqueta" y "findByUsuarioOrderedByFecha" en el repositorio de la entidad Nota.

--- src/Jazzyweb/AulasMentor/NotasFrontendBundle/JAMNotasFrontendBundle/Repository/NotaRepository.php
+++ src/Jazzyweb/AulasMentor/NotasFrontendBundle/JAMNotasFrontendBundle/Repository/NotaRepository.php
@@ -3,6 +3,9 @@
 namespace Jazzyweb\AulasMentor\NotasFrontendBundle\JAMNotasFrontendBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Jazzyweb\AulasMentor\NotasFrontendBundle\JAMNotasFrontendBundle\Entity\Etiqueta;
+use Jazzyweb\AulasMentor\NotasFrontendBundle\JAMNotasFrontendBundle\Entity\Usuario;
+
 
 /**
  * NotaRepository
@@ -12,4 +15,66 @@ use Doctrine\ORM\EntityRepository;
  */
 class NotaRepository extends EntityRepository
 {
+
+    public function findByUsuarioAndEtiqueta($usuario, $etiqueta)
+    {
+        if ($usuario instanceof Usuario) {
+            $username = $usuario->getUsername();
+        } else {
+            $username = $usuario;
+        }
+
+        if ($etiqueta instanceof Etiqueta) {
+            $id_etiqueta = $etiqueta->getId();
+        } else {
+            $id_etiqueta = $etiqueta;
+        }
+
+        $query = $this->getEntityManager()
+            ->createQuery(
+                "SELECT n FROM JAMNotasFrontendBundle:Nota n
+             JOIN  n.etiquetas e
+             JOIN n.usuario u
+             WHERE e.id = :id_etiqueta and u.username=:username")
+            ->setParameters(array(
+                    'username' => $username,
+                    'id_etiqueta' => $id_etiqueta)
+            );
+
+        return $query->getResult();
+    }
+
+    public function findByUsuarioOrderedByFecha($usuario)
+    {
+        $query = $this->getEntityManager()->createQuery(
+            "SELECT n FROM JAMNotasFrontendBundle:Nota n
+                          JOIN n.usuario u
+                          WHERE  u.username=:username ORDER BY n.fecha DESC")
+            ->setParameters(array('username' => $usuario->getUsername()));
+
+        return $query->getResult();
+    }
+
+    public function findByUsuarioAndTermino($usuario, $termino)
+    {
+        if ($usuario instanceof Usuario) {
+            $username = $usuario->getUsername();
+        } else {
+            $username = $usuario;
+        }
+
+        $query = $this
+            ->getEntityManager()
+            ->createQuery(
+                "SELECT n FROM JAMNotasFrontendBundle:Nota n
+             JOIN n.usuario u
+             WHERE u.username=:username AND (n.texto LIKE :termino OR n.titulo LIKE :termino)"
+            )
+            ->setParameters(array(
+                    'username' => $username,
+                    'termino' => '%' . $termino . '%')
+            );
+
+        return $query->getResult();
+    }
 }
